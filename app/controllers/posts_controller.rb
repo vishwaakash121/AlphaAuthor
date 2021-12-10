f0c479b3c19432edb19129e0f36e1ff2c7017022
akash.vishwakarma <akash.vishwakarma>
2021-12-10 20:26:05 +0530

    refactor

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -44,7 +44,7 @@ class PostsController < ApplicationController
   private
 
   def set_post
-    @post = post.find(params[:id])
+    @post = Post.find(params[:id])
   end
 
   def post_params
--- db/migrate/20211119073320_create_posts.rb
+++ db/migrate/20211119073320_create_posts.rb
@@ -2,8 +2,12 @@ class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
       t.string :title
+      t.date :date
+      t.string :author
       t.text :description
+      t.string :tags
       t.integer :user_id
+      t.timestamps
     end
   end
 end
