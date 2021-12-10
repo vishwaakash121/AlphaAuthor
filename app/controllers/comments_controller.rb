1d176169d8ddfc605386cd2128b3432dc8834695
akash.vishwakarma <akash.vishwakarma>
2021-12-10 19:56:46 +0530

    implemented base architect and views for basic feature

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,59 @@
+class CommentsController < ApplicationController
+  before_action :set_comment, only: %i[edit update destroy]
+  before_action :require_user, only: %i[edit update destroy]
+  before_action :require_same_user, only: %i[edit update destroy]
+
+  def new
+    @post_id = params[:post_id]
+    @comment = Comment.new
+  end
+
+  def show; end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.user_id = current_user.id
+    @comment.post_id = params[:post_id]
+
+    if @comment.save
+      redirect_to post_path(@comment.post)
+    else
+      flash[:notice] = 'Something Went Wrong.'
+      render 'new'
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @comment.update(comment_params)
+      flash[:notice] = 'Comment was updated successfully.'
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment)
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+    @post = Post.find(@comment.post_id)
+  end
+
+  def require_same_user
+    if current_user != @comment.user
+      flash[:alert] = 'You can only edit or delete your own comment.'
+      redirect_to @post
+    end
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  has_many :comments, dependent: :destroy
+  has_many :posts, dependent: :destroy
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :username, uniqueness: { case_sensitive: false },
+                       presence: true, length: { minimum: 3, maximum: 25 }
+  validates :email, uniqueness: { case_sensitive: false },
+                    presence: true, length: { maximum: 105 },
+                    format: { with: VALID_EMAIL_REGEX }
+
+  has_secure_password
+end
